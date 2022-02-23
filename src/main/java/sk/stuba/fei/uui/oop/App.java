4e519218a724fe6efbc7908fd3e32e696f56f6e5
Matus_Vano <xvanom@stuba.sk>
2022-02-23 15:14:07 +0100

    Zmena

--- src/main/java/sk/stuba/fei/uui/oop/App.java
+++ src/main/java/sk/stuba/fei/uui/oop/App.java
@@ -2,6 +2,31 @@ package sk.stuba.fei.uui.oop;
 
 public class App {
     public static void main(String[] args){
+
     System.out.println("Hello world");
+     /*
+        String firstName = "John ";
+        String lastName = "Doe";
+        System.out.println(firstName.concat(lastName));
+        System.out.println(firstName.indexOf("J"));
+        System.out.println(Math.random());
+        int cislo=55;
+        String result=(cislo>10)? "Ahoj" : "Chod prec";
+        System.out.println(result);
+
+
+        try
+        {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[2]);
+        }
+        catch
+        (Exception e) {
+            System.out.println("Something went wrong.");
+        }
+        finally{
+            System.out.println("Everything's fine");
+        }
+        */
     }
 }
